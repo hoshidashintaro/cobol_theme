fdad0031a42fee5187c67e48d047fe9c42c0440d
hoshidashintaro <shin03ho09@gmail.com>
2023-07-21 16:21:12 +0900

    IN01-ZYUTYU-FILE-READ-PROCに前レコードの集計キー保存を追加

--- Suma/summary.cob
+++ Suma/summary.cob
@@ -128,11 +128,14 @@
        IN01-ZYUTYU-FILE-READ-PROC                          SECTION.
        *>
            READ IN01-ZYUTYU-FILE
-           AT END
-           MOVE "END" TO WRK-AT-END
-           DISPLAY "READ END"
+                AT     END
+                MOVE   "END"             TO   WRK-AT-END
+                DISPLAY "READ END"
            *>
-           NOT AT END
+               NOT   AT   END
+               MOVE   IN01-BUNRUI-CODE   TO   KEY-BUNRUI-CODE
+               MOVE   IN01-SHOHIN-NO     TO   KEY-SHOHIN-NO
+           END-READ.
 
        *>
        IN01-ZYUTYU-FILE-READ-PROC-EXIT.
